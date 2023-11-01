8f5012e0802b3cf4393c31fec90f42552fceeb50
Michael O <michaelo@Michaels-MacBook-Pro.local>
2023-10-31 23:16:57 -0400

    small change to eval.ml

--- data.ml
+++ data.ml
@@ -10,14 +10,18 @@ and value = Int of int | Field of string | Closure of closure | Location of loca
 
 and tainted_value = Value of value | RuntimeError
 
+and env = Env of (string * location) list
+
+and call = Call of (env * stack)
+
 and frame = Frame of (string * location) list
 
 and stack = Stack of frame list
 
 and heap = Heap of ((location * string) * tainted_value) list
 
-and state = State of (stack * heap)
+and state = State of stack * heap
 
 and control = Cmd of cmd | Block of cmd
 
-and conf = ControlAndState of (control * state) | State of state | RuntimeError
+and conf = ControlAndState of (control * state) | OnlyState of state | RuntimeError
--- eval.ml
+++ eval.ml
@@ -2,12 +2,14 @@ open Ast;;
 open Data;;
 
 let rec eval_cmd c s = match c, s with
-	| Declare(s, c1), State(st, hp) 
-							->  let rec l = Object(0)
-							    and f = Frame([(s, l)])
-					            and st' = f::st
-					            and hp' = ((l, "val"), Value(Location(Null)))::hp
-					            in (Block(c1), State(st', hp'))
+	| Declare(v, c1), 
+	  State(Stack(st), Heap(hp)) 
+					->  let rec l = Object(0)
+						and f = Frame([(v, l)])
+					    and st' = f::st
+					    and hp' = ((l, "val"), Value(Location(Null)))::hp
+					    in (Block(c1), 
+					    	State(Stack(st'), Heap(hp')))
 
 and eval_expr e s = match e with
 	| Minus(e1, e2) -> true
