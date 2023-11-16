7ec7f763fce58aa060e830cd180f892e013d6c2c
Michael O <michaelo@Michaels-MacBook-Pro.local>
2023-11-16 11:31:52 -0500

    fixed spaces

--- eval.ml
+++ eval.ml
@@ -34,29 +34,29 @@ and eval_cmd (c: cmd) (s: state) = match c, s with
 
   | Sequence(c1, c2), 
   	State(st, hp) -> let result_conf = eval_cmd c1 (State(st, hp))
-										 in
-										 match result_conf with
-										 | ControlAndState(Control(c1'), State(st', hp'))
-										 		-> eval_cmd (Sequence(c1', c2)) (State(st', hp'))
-										 | ControlAndState(Block(c1'), State(st', hp'))
-										 		-> eval_cmd (Sequence(c1', c2)) (State(st', hp'))
-										 | FinalState(State(st', hp')) 
-										 		-> eval_cmd c2 (State(st', hp'))
-										 | ProgramError -> ProgramError
+                      in
+                      match result_conf with
+                      | ControlAndState(Control(c1'), State(st', hp'))
+                      		-> eval_cmd (Sequence(c1', c2)) (State(st', hp'))
+                      | ControlAndState(Block(c1'), State(st', hp'))
+                      		-> eval_cmd (Sequence(c1', c2)) (State(st', hp'))
+                      | FinalState(State(st', hp')) 
+                      		-> eval_cmd c2 (State(st', hp'))
+                      | ProgramError -> ProgramError
 
 
 and eval_expr e s = match e, s with
-	| Num(i), _ -> Value(IntVal(i))
-	| Ident(name), State(st, hp) -> let loc = get_location name st
-																	in
-																	get_val_from_heap loc "val" hp
-	| Minus(e1, e2), s -> let v1 = eval_expr e1 s
-											  and v2 = eval_expr e2 s
-											  in 
-											  match v1, v2 with 
-											  | Value(IntVal(i)), Value(IntVal(j)) -> Value(IntVal(i - j))
-											  | _, _ -> ValueError
-	| _,_ -> Value(IntVal(0))
+  | Num(i), _ -> Value(IntVal(i))
+  | Ident(name), State(st, hp) -> let loc = get_location name st
+  																in
+  																get_val_from_heap loc "val" hp
+  | Minus(e1, e2), s -> let v1 = eval_expr e1 s
+  										  and v2 = eval_expr e2 s
+  										  in 
+  										  match v1, v2 with 
+  										  | Value(IntVal(i)), Value(IntVal(j)) -> Value(IntVal(i - j))
+  										  | _, _ -> ValueError
+  | _,_ -> Value(IntVal(0))
 
 (* define function equality / less_than to check for errors
 with pattern matching
@@ -64,10 +64,10 @@ with pattern matching
 	_, Error2 -> Error
 *)
 and eval_bool b s = match b with
-	| BoolValue(b1) -> Bool(b1)
-	| Equals(e1, e2) -> let rec v1 = eval_expr(e1)
-						and v2 = eval_expr(e2)
-						in Bool(v1 = v2)
-	| LessThan(e1, e2) -> let rec v1 = eval_expr(e1)
-						  and v2 = eval_expr(e2)
-						  in Bool(v1 < v2)
+  | BoolValue(b1) -> Bool(b1)
+  | Equals(e1, e2) -> let rec v1 = eval_expr(e1)
+  					and v2 = eval_expr(e2)
+  					in Bool(v1 = v2)
+  | LessThan(e1, e2) -> let rec v1 = eval_expr(e1)
+  					  and v2 = eval_expr(e2)
+  					  in Bool(v1 < v2)
